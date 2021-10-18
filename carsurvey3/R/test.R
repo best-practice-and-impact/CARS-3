15a2d31964a037f476de2bbd162e70a3ccfba366
Dan Shiloh <daniel.shiloh@ons.gov.uk>
2021-10-18 12:32:51 +0100

    test function and unit test

---
+++ carsurvey3/R/test.R
@@ -0,0 +1,11 @@
+#' @title Test function 
+#'
+#' @description Test function used to test github actions functionality. Delete when other functionality is added. 
+#' 
+#' @returns TRUE
+#' 
+#' @export
+
+test <- function() {
+  return(TRUE)
+}
