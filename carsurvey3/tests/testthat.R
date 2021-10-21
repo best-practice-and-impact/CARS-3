53924e74244414e6d2af8dee64f91075756a011e
Dan Shiloh <daniel.shiloh@ons.gov.uk>
2021-10-21 09:52:46 +0100

    bug fix

--- carsurvey3/tests/testthat.R
+++ carsurvey3/tests/testthat.R
@@ -2,6 +2,7 @@ library(testthat)
 library(carsurvey3)
 
 tryCatch(
+  library(package = carsurvey2),
   error = function(e) {
     library(carsurvey2, lib.loc = "/Users/runner/work/_temp/Library")
   }
